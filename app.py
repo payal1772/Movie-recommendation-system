f90156eed8129ff456beca887ca78912026e53a3
Patil Payal <payalpatil1772@gmail.com>
2026-02-26 15:04:22 +0530

    Fixed select box in app.py

--- app.py
+++ app.py
@@ -2,46 +2,61 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+# ---------------- PAGE CONFIG ----------------
+st.set_page_config(page_title="Movie Recommender", page_icon="🎬")
+
+st.title("🎬 Movie Recommendation System")
+st.write("Select a movie and get similar movie suggestions!")
+
 # ---------------- LOAD FILES ----------------
-# Use st.cache_data so the app doesn't reload large files every time you click a button
 @st.cache_data
 def load_data():
-    movies = pickle.load(open('movie_list.pkl','rb'))   # movie dataframe
-    similarity = pickle.load(open('similarity.pkl','rb'))  # similarity matrix
+    movies = pickle.load(open('movie_list.pkl','rb'))
+    similarity = pickle.load(open('similarity.pkl','rb'))
     return movies, similarity
 
 movies, similarity = load_data()
 
 # ---------------- RECOMMEND FUNCTION ----------------
 def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
+
+    movie_index = movies[movies['title'] == movie].index
+
+    # Safety check
+    if len(movie_index) == 0:
+        return ["Movie not found"]
+
+    movie_index = movie_index[0]
+
     distances = similarity[movie_index]
-    # Get top 5 similar movies
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    # Top 5 similar movies
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
     recommended_movies = []
+
     for i in movies_list:
         recommended_movies.append(movies.iloc[i[0]].title)
+
     return recommended_movies
 
-# ---------------- UI DESIGN ----------------
-st.set_page_config(page_title="Movie Recommender", page_icon="🎬")
 
-st.title("🎬 Movie Recommendation System")
-st.write("Select a movie and get similar movie suggestions!")
+# ---------------- UI ----------------
 
-# 🎥 Dropdown with all movie titles
-# Added a unique key just in case, though removing the duplicate code fixes the main issue
 selected_movie = st.selectbox(
     "Choose a movie",
-    movies['title'].values,
-    key="movie_dropdown"
+    movies['title'].values
 )
 
-# 🔘 Button
-if st.button("Recommend Movies", key="recommend_button"):
+if st.button("Recommend Movies"):
+
     recommendations = recommend(selected_movie)
+
     st.subheader("✨ Recommended for you:")
-    
+
     for movie in recommendations:
         st.write("👉", movie)
